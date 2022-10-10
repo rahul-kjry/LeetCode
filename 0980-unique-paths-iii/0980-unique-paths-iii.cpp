3536ada0017d5a498a9750371293fdf1ed360bc3
RAHUL K <76773801+rahulkjry@users.noreply.github.com>
2022-10-10 22:05:34 +0530

    Time: 7 ms (42.06%), Space: 7 MB (92.01%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int rows, cols;
-    vector<vector<int>> dp;
     int g(vector<vector<int>> & mat, int roboR, int roboC, int flagR, int flagC, int zero) {
         if (min(roboR, roboC) < 0 or roboR == rows or roboC == cols or mat[roboR][roboC] == -1) {
             return 0;
@@ -9,20 +8,18 @@ public:
         if (roboR == flagR and roboC == flagC) {
             return zero == -1;
         }
-        //if (dp[roboR][roboC] != -1) return dp[roboR][roboC];
         mat[roboR][roboC] = -1;
         int res = g(mat, roboR + 1, roboC, flagR, flagC, zero - 1) + 
             g(mat, roboR - 1, roboC , flagR, flagC, zero - 1) + 
             g(mat, roboR, roboC + 1, flagR, flagC, zero - 1) + 
             g(mat, roboR, roboC - 1, flagR, flagC, zero - 1);
         mat[roboR][roboC] = 0;
-        return dp[roboR][roboC] = res;
+        return res;
     }
     int uniquePathsIII(vector<vector<int>>& mat) {
         int roboR = -1, roboC = -1, flagR = -1, flagC = -1;
         rows = mat.size(), cols = mat[0].size();
         int zero = 0;
-        dp.resize(rows, vector<int>(cols, -1));
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 if (mat[i][j] == 1) {
