12be0f01cd19e930362a8672328ca1c262774652
RAHUL K <76773801+rahulkjry@users.noreply.github.com>
2022-10-11 23:23:41 +0530

    Time: 1726 ms (5.81%), Space: 146.5 MB (46.09%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int rows, cols, mod = 1e9 + 7;
-    vector<vector<vector<int>>> dp;
-    
-    int g(vector<vector<int>> & mat, int i, int j, int sum, int k) {
-        if (min(i, j) < 0 or i == rows or j == cols) {
-            return 0;
-        }
-        sum += mat[i][j];
-        if (i == rows - 1 and j == cols - 1) {
-            return sum % k == 0;
-        }
-        if (dp[i][j][sum % k] != -1) return dp[i][j][sum % k];
-        return dp[i][j][sum % k] = (g(mat, i + 1, j, sum, k) % mod + 
-                                    g(mat, i, j + 1, sum, k) % mod) % mod;
-    }
-    
     int numberOfPaths(vector<vector<int>>& mat, int k) {
-        rows = mat.size(), cols = mat[0].size();
-        dp.resize(rows, vector<vector<int>>(cols, vector<int>(k, -1)));
-        return g(mat, 0, 0, 0, k);
+        int rows = mat.size(), cols = mat[0].size(), mod = 1e9 + 7;
+        vector<vector<vector<int>>> dp(rows, vector<vector<int>>(cols, vector<int>(k, -1)));
+        function<int(int, int, int, int)> g = [&](int i, int j, int sum, int k){
+            if (min(i, j) < 0 or i == rows or j == cols) {
+                return 0;
+            }
+            sum += mat[i][j];
+            if (i == rows - 1 and j == cols - 1) {
+                return (sum % k == 0 ? 1 : 0);
+            }
+            if (dp[i][j][sum % k] != -1) return dp[i][j][sum % k];
+            return dp[i][j][sum % k] = (g(i + 1, j, sum, k) % mod + 
+                                        g(i, j + 1, sum, k) % mod) % mod;
+        };
+        return g(0, 0, 0, k);
     }
 };
