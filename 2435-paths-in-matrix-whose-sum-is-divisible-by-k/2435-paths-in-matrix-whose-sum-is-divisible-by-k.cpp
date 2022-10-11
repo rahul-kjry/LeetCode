f4cd84e511b93982fa6fd3ab8da001a3c9bca35b
RAHUL K <76773801+rahulkjry@users.noreply.github.com>
2022-10-11 23:13:26 +0530

    Time: 1241 ms (36.81%), Space: 132.3 MB (72.17%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int rows, cols, rs = 0;
+    int rows, cols, mod = 1e9 + 7;
     vector<vector<vector<int>>> dp;
-    int md = 1e9 + 7;
-    int g(vector<vector<int>>& mat, int i, int j, int sum, int k) {
-        if (i == rows || j == cols) return 0;
+    
+    int g(vector<vector<int>> & mat, int i, int j, int sum, int k) {
+        if (min(i, j) < 0 or i == rows or j == cols) {
+            return 0;
+        }
         sum += mat[i][j];
-        if (dp[i][j][sum % k] != -1) return dp[i][j][sum % k];
-
         if (i == rows - 1 and j == cols - 1) {
             return sum % k == 0;
         }
-        return dp[i][j][sum % k] = (g(mat, i + 1, j, sum, k) % md + 
-            g(mat, i, j + 1, sum, k) % md) % md;
+        if (dp[i][j][sum % k] != -1) return dp[i][j][sum % k];
+        return dp[i][j][sum % k] = (g(mat, i + 1, j, sum, k) % mod + 
+                                    g(mat, i, j + 1, sum, k) % mod) % mod;
     }
     
     int numberOfPaths(vector<vector<int>>& mat, int k) {
