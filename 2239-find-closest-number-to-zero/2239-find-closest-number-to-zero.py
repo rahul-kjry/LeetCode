d85bc48fa23b02e653c32f41666b9a82a068f84a
RAHUL K <76773801+rahulkjry@users.noreply.github.com>
2022-10-12 15:59:14 +0530

    Time: 392 ms (18.43%), Space: 14.1 MB (91.66%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.py
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.py
@@ -1,12 +1,3 @@
-class Solution {
-public:
-    int findClosestNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), [](int & a, int & b){
-            return abs(a) < abs(b);
-        });
-        if (find(nums.begin(), nums.end(), abs(nums[0])) != nums.end()) {
-            return abs(nums[0]);
-        }
-        return nums[0];
-    }
-};
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        return min(nums, key = lambda x: (abs(x),-x))
