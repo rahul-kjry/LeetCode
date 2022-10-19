2939547ca0455c08ea1c9c06e959e9ae14b4afbf
RAHUL K <76773801+rahulkjry@users.noreply.github.com>
2022-10-19 14:37:57 +0530

    Time: 1180 ms (10.86%), Space: 242.3 MB (5.91%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
+    
+    void merge(vector<int> & vec, int lo, int md, int hi){
+        int i = lo, j = md + 1;
+        vector<int> tmp;
+        while (i <= md and j <= hi ) tmp.push_back(vec[i] <= vec[j] ? vec[i++] : vec[j++]);
+        while (i <= md) tmp.push_back(vec[i++]);
+        while (j <= hi) tmp.push_back(vec[j++]);
+        for (int i = lo; i <= hi; vec[i] = tmp[i - lo], ++i);
+    }
+
+    void mergeSort(vector<int> & vec, int lo, int hi) {
+        if (lo < hi) {
+            int md = lo + (hi - lo) / 2;
+            mergeSort(vec, lo, md);
+            mergeSort(vec, md + 1, hi);
+            merge(vec, lo, md, hi);
+        }
+    }
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        mergeSort(nums, 0, nums.size() - 1);
         return nums;
     }
 };
