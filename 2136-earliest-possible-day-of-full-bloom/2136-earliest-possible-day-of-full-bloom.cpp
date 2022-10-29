8e6225ea38e6aa90476e800d491ecb5fe71b197a
RAHUL K <76773801+rahulkjry@users.noreply.github.com>
2022-10-29 19:29:03 +0530

    Time: 755 ms (32.33%), Space: 168.7 MB (29.92%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
+    #define F first
+    #define S second
+    #define pi pair<int, int>
     int earliestFullBloom(vector<int>& pt, vector<int>& gt) {
-        vector<pair<int, int>> vp;
-        int len = pt.size();
-        for (int i = 0; i < len; ++i) {
+        vector<pi> vp;
+        for (int i = 0; i < pt.size(); ++i)
             vp.push_back({pt[i], gt[i]});
-        }
-        sort(vp.begin(), vp.end(), 
-             [](const pair<int, int> & val1, const pair<int, int> & val2){
-                 if (val1.second == val2.second) {
-                     return val1.first < val2.first;
-                 }
-                 else {
-                     return val1.second > val2.second;
-                 }
-             });
-        int pre = 0, res = 0;
-        for (auto & i : vp) {
-            pre += i.first;
-            res = fmax(res, pre + i.second);
-        }
+        sort(vp.begin(), vp.end(), [](const pi & v1, const pi & v2){
+                 return v1.S == v2.S ? v1.F < v2.F : v1.S > v2.S; });
+        int pre(0), res(0);
+        for (auto & i : vp)
+            res = fmax(res, (pre += i.F) + i.second);
         return res;
     }
 };
