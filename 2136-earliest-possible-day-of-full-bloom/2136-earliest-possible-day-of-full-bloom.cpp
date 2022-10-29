5dfe28ccb40f6d2f7a0f2064f3d8709c15681aa6
RAHUL K <76773801+rahulkjry@users.noreply.github.com>
2022-10-29 19:19:25 +0530

    Time: 350 ms (91.37%), Space: 168.7 MB (29.92%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int earliestFullBloom(vector<int>& pt, vector<int>& gt) {
+        vector<pair<int, int>> vp;
+        int len = pt.size();
+        for (int i = 0; i < len; ++i) {
+            vp.push_back({pt[i], gt[i]});
+        }
+        sort(vp.begin(), vp.end(), 
+             [](const pair<int, int> & val1, const pair<int, int> & val2){
+                 if (val1.second == val2.second) {
+                     return val1.first < val2.first;
+                 }
+                 else {
+                     return val1.second > val2.second;
+                 }
+             });
+        int pre = -1, res = 0;
+        for (auto & i : vp) {
+            pre += i.first;
+            res = fmax(res, pre + i.second + 1);
+        }
+        return res;
+    }
+};
