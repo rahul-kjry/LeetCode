805aa5dd253ac0eeb73699779de0ab06582a1937
RAHUL K <76773801+rahulkjry@users.noreply.github.com>
2022-11-04 15:50:59 +0530

    Time: 177 ms (84.01%), Space: 22.6 MB (59.91%) - LeetHub

---
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int rows, cols;
+    int dp[101][101][205];
+    bool g(vector<vector<char>> & mat, int i, int j, int bal) {
+        if (i == rows or j == cols or bal < 0) {
+            return 0;
+        }
+        bal += (mat[i][j] == '(' ? 1 : -1);
+        if (bal < 0) {
+            return 0;
+        }
+        if (i == rows - 1 and j == cols - 1 and bal == 0) {
+            return 1;
+        } 
+        if (dp[i][j][bal] != -1) {
+            return dp[i][j][bal];
+        }
+        return dp[i][j][bal] = g(mat, i + 1, j, bal) or g(mat, i, j + 1, bal);
+    }
+    
+    bool hasValidPath(vector<vector<char>>& mat) {
+        rows = mat.size(), cols = mat[0].size();
+        memset(dp, -1, sizeof(dp));
+        return g(mat, 0, 0, 0);
+    }
+};
