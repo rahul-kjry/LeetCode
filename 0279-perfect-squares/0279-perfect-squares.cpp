1d5503c3c0a951d5601e7ac4bd4534eae0995f13
RAHUL K <76773801+rahul-kjry@users.noreply.github.com>
2022-11-22 18:10:18 +0530

    Time: 388 ms (52.95%), Space: 8.8 MB (84.73%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    int numSquares(int num) {
-        vector<int> dp(num + 1, INT_MAX);
-        dp[0] = 0;
-        int cnt = 1;
-        while (cnt * cnt <= num) {
-            for (int i = cnt * cnt; i <= num; ++i) {
-                dp[i] = min(dp[i], dp[i - cnt * cnt] + 1);
+    int dp[10001];
+    int numSquares(int n) {
+        function<int(int)> g = [&](int tar) {
+            if (tar == 0) {
+                return 0;
             }
-            cnt += 1;
-        }
-        return dp[num];
+            if (tar < 0) {
+                return INT_MAX;
+            }
+            if (dp[tar] != -1) {
+                return dp[tar];
+            }
+            int min = INT_MAX;
+            for (int i = 1; i * i <= tar; ++i) {
+                min = fmin(min, 1 + g(tar - i * i));
+            }
+            return dp[tar] = min;
+        };
+        memset(dp, -1, sizeof(dp));
+        return g(n);
     }
 };
