0b88b3dc0ee66898af5cc75807d67d84cb381952
RAHUL K <76773801+rahul-kjry@users.noreply.github.com>
2022-11-23 18:28:55 +0530

    Time: 41 ms (65.20%), Space: 18.1 MB (72.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& mat) {
-        vector<int> rows(9), cols(9), box(9);
+    bool isValidSudoku(vector<vector<char>> & mat) {
+        vector<int> rows(9), cols(9), boxs(9);
         for (int i = 0; i < 9; ++i) {
             for (int j = 0; j < 9; ++j) {
                 if (mat[i][j] == '.') 
                     continue;
                 int msk = 1 << (mat[i][j] & 0xf);
-                if (rows[i] & msk)
+                int a = i / 3 * 3 + j / 3;
+                if (rows[i] & msk or cols[j] & msk or boxs[a] & msk)
                     return false;
-                rows[i] ^= msk;
-                if (cols[j] & msk) 
-                    return false;
-                cols[j] ^= msk;
-                if (box[i / 3 * 3 + j / 3] & msk) 
-                    return false;
-                box[i / 3 * 3 + j / 3] ^= msk;
+                rows[i] ^= msk, cols[j] ^= msk, boxs[a] ^= msk;
             }
         }
         return true;
